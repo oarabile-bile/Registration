82aa925eaacb37adb57f1b5d47311df757a3a60b
oarabile-bile <ST10465359@rcconnect.edu.za>
2026-05-07 22:32:27 +0200

    Updated Registration.java with the messaging logic

--- src/main/java/com/mycompany/registration/Registration.java
+++ src/main/java/com/mycompany/registration/Registration.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.registration;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 /**
  *
  * @author Student
@@ -13,6 +14,35 @@ public class Registration {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Login loginLogic = new Login();
+        int mainChoice;
+        
+        do{
+            System.out.println("\n=== REGISTER & LOGIN SYSTEM ===");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
+            mainChoice = input.nextInt();
+            input.nextLine(); 
+            switch (mainChoice) {
+                case 1:
+                    runRegistration(input, loginLogic);
+                    break;
+                case 2:
+                    runLoginAndChat(input, loginLogic);
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } while (mainChoice != 3);
+
+        input.close();
+    }
+    
+    public static void runRegistration(Scanner input, Login loginLogic){
         
         //Registration
         System.out.println("\n---Registration---");
@@ -34,9 +64,11 @@ public class Registration {
         
         String regStatus = loginLogic.registerUser();
         System.out.println("\nRegistration Status: " + regStatus);
-        
+    }
+    
+    public static void runLoginAndChat(Scanner input, Login loginLogic){
         //Login
-        if(regStatus.equals("Username and Password successfully captured.")){
+     
             System.out.print("\n---Login---");
             
             System.out.print("Enter Username: ");
@@ -48,9 +80,73 @@ public class Registration {
             String loginStatus = loginLogic.returnLoginStatus(enteredUsername, enteredPassword);
             System.out.println("\n" + loginStatus);
             
-        }else{
-            System.out.println("\nPlease fix the errors above and try again.");
+            if(loginLogic.loginUser(enteredUsername, enteredPassword)){
+                showQuickChatMenu(input);
+            }
+    }
+    
+    public static void showQuickChatMenu(Scanner input){
+        int choice;
+        
+        do{
+            System.out.println("\n===QUICKCHAT MENU===");
+            
+            System.out.println("1. Send Messages");
+            System.out.println("2. Show recently sent messages");
+            System.out.println("3. Logout");
+            System.out.println("Choose option: ");
+            
+            choice = input.nextInt();
+            input.nextLine();
+            
+            if(choice == 1){
+                initializeMessaging(input);
+            }else if(choice == 2 ){
+                System.out.println("Coming Soon");
+            }
+            
+        }while(choice != 3);
+    }
+    
+    public static void initializeMessaging(Scanner input){
+        
+        System.out.print("\nHow many messages do you want to send?");
+        
+        int num = input.nextInt();
+        input.nextLine();
+        int sentCount = 0;
+        
+        for(int i = 0; i < num; i++){
+             System.out.println("\n--- Message " + i + " ---");
+             
+            System.out.print("Enter recipient: ");
+            String recipient = input.nextLine();
+            
+            System.out.print("Enter message (max 250): ");
+            String text = input.nextLine();
+
+            if (text.length() > 250) {
+                System.out.println("Message too long!");
+                continue;
+            }
+
+            Message message = new Message(i, recipient, text);
+            System.out.println(message.checkRecipientCell());
+            
+            if(message.checkRecipientCell().contains("successflly")){
+                System.out.println("1, Send 2. Disregard 3. Store");
+                
+                int action = input.nextInt();
+                input.nextLine();
+                
+                if(action == 1){
+                    sentCount++;
+                    System.out.println("Message Sent!");
+                    System.out.println("ID: " + message.getMessageId());
+                    System.out.println("Hash: " + message.createMessageHash());
+                }
+            }
         }
-        input.close();
+        System.out.println("\nTotal messages sent in this session: " + sentCount);
     }
 }
